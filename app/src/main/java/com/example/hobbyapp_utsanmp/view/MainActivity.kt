892e4ee05bdf8b1a431c564f112a017e6840b5d0
Anri20 <88647934+Anri20@users.noreply.github.com>
2024-04-08 17:42:24 +0700

    Initial Setup

---
+++ app/src/main/java/com/example/hobbyapp_utsanmp/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hobbyapp_utsanmp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hobbyapp_utsanmp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
